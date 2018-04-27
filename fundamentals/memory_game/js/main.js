256ac5a3fcab0b95fd041d234c5b4f96914c101f
Antoine Randle <RandleAntoine@gmail.com>
2018-04-27 16:12:31 -0700

    Add files via upload

--- fundamentals/memory_game/js/main.js
+++ fundamentals/memory_game/js/main.js
@@ -1,1 +1,8 @@
 console.log("Up and running!");
+// Memory Card Game JS code
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
